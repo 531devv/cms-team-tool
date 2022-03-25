1e18ce99b4f6111fb420aee496ce1d3d902984c5
Marek Klatt <marek.klatt@MacBook-Pro-Marek-Klatt.local>
2022-03-25 22:56:19 +0100

    Adding js function to cropping images

--- js/script.js
+++ js/script.js
@@ -1,13 +1,39 @@
-function cropImg(){
+function cropImg(imageSrc, width, height){
+    this.src = imageSrc;
+    this.width = width;
+    this.height = height;
+    
     const canvas = document.getElementById('canvas');
     const ctx = canvas.getContext('2d');
-  
+    ctx.canvas.width  = width;
+    ctx.canvas.height = height;
     var image = new Image();
-    image.src = "https://images.unsplash.com/photo-1593642634443-44adaa06623a?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=725&q=80"; 
+    image.src = imageSrc; 
     //drawImage(image, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight);
     image.onload = function(){
-      ctx.drawImage(image, 0, 0, 500, 300, 60, 60, 500, 300);
+        var sourceX;
+        var sourceY;
+        if(image.width != width) {
+          sourceX = (image.width - width) / 2;
+        } else {
+          sourceX = 0;
+        }
+        if(!image.height != height) {
+          sourceY = (image.width - height) / 2;
+        } else {
+          sourceY = 0;
+        }
+        var sourceWidth = width;
+        var sourceHeight = height;
+        var displayX = 0;
+        var displayY = 0;
+        var displayWidth = width;
+        var displayHeight = height;
+        ctx.drawImage(image, sourceX, sourceY, sourceWidth, sourceHeight, displayX, displayY, displayWidth, displayHeight);
+        var img = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");  // here is the most important part because if you dont replace you will get a DOM 18 exception.
+        //window.location.href=img; // it will save locally
     }
   }
-  
-  cropImg();
+
+
+cropImg('./2.png', 400, 970);
